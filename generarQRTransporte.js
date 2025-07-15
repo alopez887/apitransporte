76fbfcb32fc60f295d30f14c92e6e08a68504fb5
alberto <nkmsistemas@gmail.com>
2025-07-15 10:17:22 -0700

    Actualización: Separar Login de Validar-QR

--- generarQRTransporte.js
+++ generarQRTransporte.js
@@ -3,7 +3,7 @@ import QRCode from 'qrcode';
 export async function generarQRTransporte(token) {
   try {
     // URL que se usará en el QR con el parámetro type=transporte
-    const url = `https://nkmsistemas.wixsite.com/cabo-travel-activiti/validar-qr?token=${token}&type=transporte`;
+    const url = `https://nkmsistemas.wixsite.com/cabo-travel-activiti/login?token=${token}&type=transporte`;
 
     // Generar el QR en formato data URL
     const qrDataUrl = await QRCode.toDataURL(url);
