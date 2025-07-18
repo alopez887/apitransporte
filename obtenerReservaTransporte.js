06ccc03dc4f413f393e6e7402d655879b63eeba3
alberto <nkmsistemas@gmail.com>
2025-07-18 11:30:46 -0700

    Actualización mplementacion enfoque llegada, salida y redondo BD 2.3

--- obtenerReservaTransporte.js
+++ obtenerReservaTransporte.js
@@ -10,20 +10,28 @@ export async function obtenerReservaTransporte(req, res) {
   try {
     const query = `
       SELECT 
-        folio, tipo_viaje, tipo_transporte, hotel_llegada, hotel_salida, zona, capacidad,
+        folio, tipo_viaje, tipo_transporte, zona, capacidad,
         cantidad_pasajeros, codigo_descuento, total_pago, nombre_cliente, correo_cliente, 
         telefono_cliente, nota, fecha_llegada, hora_llegada, aerolinea_llegada, vuelo_llegada,
         fecha_salida, hora_salida, aerolinea_salida, vuelo_salida, tipo_servicio, 
         porcentaje_descuento, precio_servicio, fecha, estatus, proveedor, folio_proveedor, 
-        usuario_proveedorllegada, usuario_proveedorsalida, fecha_reservacionllegada, fecha_reservacionsalida,
-        fecha_inicioviajellegada, fecha_finalviajellegada, comentariosllegada, comentariossalida,
-        firma_cliente_llegada, firma_cliente_salida,
-        choferllegada, chofersalida,
-        estatus_viaje_llegada, estatus_viajesalida
+        fecha_reservacion,
+
+        -- Llegada
+        usuario_proveedorllegada, fecha_inicioviajellegada, fecha_finalviajellegada, 
+        comentariosllegada, firma_clientellegada, choferllegada, numero_unidadllegada, 
+        estatus_viajellegada, cantidad_pasajerosokllegada,
+
+        -- Salida
+        usuario_proveedorsalida, fecha_inicioviajesalida, fecha_finalviajesalida, 
+        comentariossalida, firma_clientesalida, chofersalida, numero_unidadsalida, 
+        estatus_viajesalida, cantidad_pasajerosoksalida
+
       FROM reservaciones
       WHERE token_qr = $1
       LIMIT 1
     `;
+
     const result = await pool.query(query, [token]);
 
     if (result.rows.length === 0) {
